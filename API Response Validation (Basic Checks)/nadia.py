9e4f6a4283795967f78d1169317a1b7a5166a5f5
sahilmalav <sahil.malav@abzooba.com>
2019-01-17 12:24:21 +0530

    Added CSV support and reorganised results

--- API Response Validation (Basic Checks)/nadia.py
+++ API Response Validation (Basic Checks)/nadia.py
@@ -1,5 +1,9 @@
 import json
 import requests
+import csv
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 with open('query_Nadia.txt') as f:
     content = f.readlines()
@@ -9,26 +13,30 @@ url = 'https://abz-nadia.southcentralus.cloudapp.azure.com:443/api/mss/advanced-
 
 headers = {'Authorization': 'Basic Y2FwaXRhbDEyMzpjYXBpdGFsQDEyMw==', 'Content-Type': 'application/json'}
 
-temp = []
-bugs = []
+temp = [('QUERY', 'DOC INDEX', 'TITLE', 'DOC ID (DOC NAME)', 'JDOC ID (CORPUS ID)', 'SECTION ID (SNIPPET ID)',\
+         'FILE TYPE', 'RELEVANCE SCORE', 'PUBLICATION FROM DATE', 'PUBLICATION TO DATE')]
+bugs = [('QUERY', 'STATUS')]
 
 for i in content:
     data = {
-        'query': i,
-        'request': {
-            'query_type': 'as',
-            'session_id': 'c494d392eb7cf63bd59e8bbe44fb8f80250c0dd7',
-            'user_id': 'capital123',
-            'search_type': 1
-        },
-        'is_nlp': 'true',
-        'source': [],
-        'file_type': [],
-        'offset': 1,
-        'time_filter_value': 0,
-        'sort_order': 'relevance'
+            "query": i,
+            "request":  {
+                        "query_type": "as",
+                        "session_id": "c494d392eb7cf63bd59e8bbe44fb8f80250c0dd7",
+                        "user_id": "capital123",
+                        "search_type": 1
+                        },
+            "is_nlp": True,
+            "source": [],
+            "file_type": [],
+            "offset": 1,
+            "time_filter_value": 0,
+            "sort_order": "relevance",
+            "apply_filter": True,
+            "publication_from_date": "",
+            "publication_to_date": "",
+            "query_id": 0
     }
-
     res = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
     json_data = json.loads(res.text)
     flag = False
@@ -38,31 +46,37 @@ for i in content:
 
         for j in json_data["records"]:
             if len(j["snippets"]) == 1 and flag and j["title"] and j["edam_date"]:
-                print(json_data['query'], j["docIndex"], True)
-                l = (json_data['query'], j["docIndex"], True)
+                l = (str(json_data['query']), j['docIndex'], str(j['title']), str(j["doc_id"]), j["jdoc_id"], \
+                     str(j["snippets"][0]["section_id"]), str(j['path'][0]['type']), j["score"], \
+                     str(json_data['publication_from_date']), str(json_data['publication_to_date']))
+                print(l)
                 if l:
                     temp.append(l)
                 else:
-                    k = (json_data['query'], "ATTENTION!!!! BUG", False)
+                    k = (json_data['query'], json_data['status'])
                     bugs.append(k)
 
             else:
-                print(json_data['query'], j["docIndex"], False)
-                l = (json_data['query'], j["docIndex"], False)
+                print(json_data['query'], json_data['status'])
+                l = (json_data['query'], json_data['status'])
                 if l:
                     temp.append(l)
                 else:
-                    k = (json_data['query'], "ATTENTION!!!! BUG", False)
+                    k = (json_data['query'], json_data['status'])
                     bugs.append(k)
 
     except:
-        q = (i, False)
+        q = (i, "BUGGY QUERY!!!")
         bugs.append(q)
 
 
-with open('Nadia_out.txt', 'w') as f:
-    for i in temp:
-        print >> f, i
-with open('buggy_Nadia_out.txt', 'w') as f:
-    for i in bugs:
-        print >> f, i
+nadia = open('Nadia_out.csv', 'w')
+with nadia:
+    writer = csv.writer(nadia)
+    writer.writerows(temp)
+
+
+nadia_bugs = open('buggy_Nadia_out.csv', 'w')
+with nadia_bugs:
+    writer = csv.writer(nadia_bugs)
+    writer.writerows(bugs)
